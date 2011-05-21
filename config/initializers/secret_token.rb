afd59504983a5fca1a9b049111b76de4f4fa29fc
Fernando D. Ramirez-Figueroa <revomail@gmail.com>
2011-05-21 10:55:23 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cgdv::Application.config.secret_token = '27cc88c37e77988338107dcdebaf3c654ddb61bdb22f96e7d945ea1444aeb9d6fad5b8f64ea4520336e0702bfb2d9b2d68d9cd759218b03ed500c9a510d97298'
