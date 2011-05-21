99f2c064ffe1f3d430a4a5b36d1a597a637f63c6
Fernando D. Ramirez-Figueroa <revomail@gmail.com>
2011-05-21 13:40:30 -0500

    Done some routing and layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  
+	# Return title on per-page basis.
+  def title
+  @base_title = "CGDV"
+    if @title.nil?
+      base_title
+    else
+	  "{base_title} | #{@title}"    	
+    end
+  end
+
+  def logo
+    image_tag("logo.jpg", :alt => "CGDV",:size => "152x62", :class => "round")
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Cgdv::Application.routes.draw do
-  get "pages/home"
+  #get "pages/home"
+  root :to => "pages#home"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
