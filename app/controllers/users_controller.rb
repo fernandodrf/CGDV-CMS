ba670511ee7a8601a7deb47566e3eb692df6b04e
Fernando D. Ramirez-Figueroa <revomail@gmail.com>
2011-05-23 17:32:47 -0500

    Modified succesful new user message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       sign_in @user
-      flash[:success] = "Succesfull Login"
+      flash[:success] = "User succesfully created."
       redirect_to @user
     else
       @title = "Sign up"
