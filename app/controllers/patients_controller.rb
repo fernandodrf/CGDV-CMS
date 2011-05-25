f77c7d034444fd803575732cdd7bd1242b59b29e
Fernando D. Ramirez-Figueroa <revomail@gmail.com>
2011-05-25 18:25:39 -0500

    Done patients edit, update, index and destroy actions

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,4 +1,10 @@
 class PatientsController < ApplicationController
+  before_filter :authenticate, :only => [:new, :create, :index, :show, :edit, :update]
+  
+  def index
+  	@title = "All Patients"
+  	@patients = Patient.paginate(:page => params[:page])
+  end
   
   def show
     @patient = Patient.find(params[:id])
@@ -8,6 +14,11 @@ class PatientsController < ApplicationController
   def new
   	@patient = Patient.new
   	@title = "New Patient"
+  end
+  
+  def edit
+    @patient = Patient.find(params[:id])
+    @title = "Edit Patient"
   end
   
   def create
@@ -20,5 +31,27 @@ class PatientsController < ApplicationController
   	  render 'new'
   	end
   end
+  
+  def update
+    @patient = Patient.find(params[:id])
+    if @patient.update_attributes(params[:patient])
+      flash[:success] = "Patient updated."
+      redirect_to @patient
+    else
+      @title = "Edit Patient"
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    Patient.find(params[:id]).destroy
+    flash[:success] = "Patient destroyed."
+    redirect_to patients_path
+  end
+  
+  private
 
+    def authenticate
+      deny_access unless signed_in?
+    end
 end
