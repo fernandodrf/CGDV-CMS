bd76aa608468e3a36bb9b2c99de314fb8899e959
Fernando D. Ramirez-Figueroa <revomail@gmail.com>
2011-05-25 17:38:20 -0500

    New patient complete

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -6,7 +6,19 @@ class PatientsController < ApplicationController
   end
 	
   def new
+  	@patient = Patient.new
   	@title = "New Patient"
+  end
+  
+  def create
+  	@patient = Patient.new(params[:patient])
+  	if @patient.save
+  	  flash[:success] = "Patient Succesfully created."
+  	  redirect_to @patient
+  	else
+  	  @title = "New Patient"
+  	  render 'new'
+  	end
   end
 
 end
