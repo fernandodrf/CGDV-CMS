dfa8b67be21634e7af3b4968272965e0295c5b2a
Fernando D. Ramirez-Figueroa <revomail@gmail.com>
2011-07-08 19:37:41 -0500

    Fixed error with gems

--- meta_search/ruby/1.8/bin/sqlite3_ruby.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-IF NOT "%~f0" == "~f0" GOTO :WinNT
-@"ruby.exe" "C:/Sites/cgdv/meta_search/ruby/1.8/bin/sqlite3_ruby" %1 %2 %3 %4 %5 %6 %7 %8 %9
-GOTO :EOF
-:WinNT
-@"ruby.exe" "%~dpn0" %*
