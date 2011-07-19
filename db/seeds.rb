0115cb257de2efe07f4e7d760d6acd48ef7a074d
Fernando D. Ramirez-Figueroa <revomail@gmail.com>
2011-07-19 14:16:26 -0500

    finished auth with can can

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,24 +7,25 @@
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
 #Catalogo Diagnosticos
-FasterCSV.foreach("#{RAILS_ROOT}/db/catalogos/diagnosticos.csv") do |row|
+require 'csv'
+CSV::Reader.parse(File.open("#{RAILS_ROOT}/db/catalogos/diagnosticos.csv", 'r')) do |row|
  if CatalogoDiagnostico.create(:diagnostico => row[0].to_s)
- 	puts "Diagnostico: #{row} cargado."
+ 	#puts row[0]
  else
- 	puts "Error al cargar #{row}."
+ 	puts "Error al cargar #{row[0]}"
  end
 end
 
 #Catalogo de Derechohabientes
-CatalogoDerechohabiente.create!(:seguro => "IMSS")
-CatalogoDerechohabiente.create!(:seguro => "ISSSTE")
-CatalogoDerechohabiente.create!(:seguro => "Sedena")
-CatalogoDerechohabiente.create!(:seguro => "Beneficencia")
-CatalogoDerechohabiente.create!(:seguro => "Sector Salud Estatal")
-CatalogoDerechohabiente.create!(:seguro => "Semar")
-CatalogoDerechohabiente.create!(:seguro => "Privado")
-CatalogoDerechohabiente.create!(:seguro => "SSGDF")
-CatalogoDerechohabiente.create!(:seguro => "SSA")
-CatalogoDerechohabiente.create!(:seguro => "Otros")
-CatalogoDerechohabiente.create!(:seguro => "ISSEMYM")
-CatalogoDerechohabiente.create!(:seguro => "Seguro Popular")
+CatalogoDerechohabiente.create(:seguro => "IMSS") ? true : puts("Error al cargar IMSS")
+CatalogoDerechohabiente.create(:seguro => "ISSSTE") ? true : puts("Error al cargar ISSSTE")
+CatalogoDerechohabiente.create(:seguro => "Sedena") ? true : puts("Error al cargar Sedena")
+CatalogoDerechohabiente.create(:seguro => "Beneficencia") ? true : puts("Error al cargar Beneficencia")
+CatalogoDerechohabiente.create(:seguro => "Sector Salud Estatal") ? true : puts("Error al cargar Sector Salud Estatal")
+CatalogoDerechohabiente.create(:seguro => "Semar") ? true : puts("Error al cargar Semar")
+CatalogoDerechohabiente.create(:seguro => "Privado") ? true : puts("Error al cargar Privado")
+CatalogoDerechohabiente.create(:seguro => "SSGDF") ? true : puts("Error al cargar SSGDF")
+CatalogoDerechohabiente.create(:seguro => "SSA") ? true : puts("Error al cargar SSA")
+CatalogoDerechohabiente.create(:seguro => "Otros") ? true : puts("Error al cargar Otros")
+CatalogoDerechohabiente.create(:seguro => "ISSEMYM") ? true : puts("Error al cargar ISSEMYM")
+CatalogoDerechohabiente.create(:seguro => "Seguro Popular") ? true : puts("Error al cargar Seguro Popular")
