3c1ca5fa0bcbdc0391ad14b8d2020239c6b44bed
Fernando D. Ramirez-Figueroa <revomail@gmail.com>
2011-11-07 22:50:45 -0600

    Added pipeline to master branch

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,12 @@
 // Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+
+//= require jquery
+//= require jquery_ujs
+//= require jquery-ui
+//= require autocomplete-rails
+//= require jquery.textareaCounter.plugin
+//= require_self
+//= require_tree .
 
 //Para clases es con .
 //Para ids es con #
