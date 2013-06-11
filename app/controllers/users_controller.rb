7d35d6a5a0c2db4f70309e82e1885d5587fdfc65
Fernando D. Ramirez F <revomail@gmail.com>
2013-06-11 22:34:01 +0200

    Correccion Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   before_filter :correct_user, :only => [:show, :edit, :update]
   before_filter :admin_user,   :only => [:new, :create, :index, :destroy]
   # Para actualizar roles
-  after_filter :update_roles, :only => [:create, :update]
+  after_filter :update_roles, :only => [:update]
 
   def index
     @title = t('user.index')
@@ -33,6 +33,16 @@ class UsersController < ApplicationController
     if @user.save
       #sign_in @user
       flash[:success] = t('flash.success.create', :model => User.to_s)
+
+      #####Actualiza roles de usuarios # muy codigo chafa, corrergir!
+      params[:ss] == "1" ? @user.add_role!('ss') : @user.remove_role!('ss')
+      params[:oficina] == "1" ? @user.add_role!('oficina') : @user.remove_role!('oficina')
+      params[:timereport] == "1" ? @user.add_role!('timereport') : @user.remove_role!('timereport')
+      params[:managetimereport] == "1" ? @user.add_role!('managetimereport') : @user.remove_role!('managetimereport')
+      params[:managedonor] == "1" ? @user.add_role!('managedonor') : @user.remove_role!('managedonor')
+      params[:managecontact] == "1" ? @user.add_role!('managecontact') : @user.remove_role!('managecontact')
+      #####
+
       #redirect_to @user
       redirect_to root_path
     else
