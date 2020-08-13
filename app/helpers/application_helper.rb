e5123c5b30cf26a6e45a328722903179260f9062
Fernando D. Ramirez F <716547+fernandodrf@users.noreply.github.com>
2020-08-13 14:07:05 +0200

    New view style for application layout and patients

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -114,4 +114,14 @@ module ApplicationHelper
     #return tt%1
   end
 
+  # Flash messages compatible with Bootstrap
+  def flash_class(level)
+    case level
+      when 'notice' then "alert alert-info"
+      when 'success' then "alert alert-success"
+      when 'error' then "alert alert-danger"
+      when 'alert' then "alert alert-warning"
+    end
+  end
+
 end
