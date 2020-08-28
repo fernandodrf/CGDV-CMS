20643d34138cfa83faac3252fd91c6c91df208b9
Fernando D. Ramirez F <716547+fernandodrf@users.noreply.github.com>
2020-08-28 15:43:47 +0200

    Changes in Patient views

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,45 +1,45 @@
 class PatientsController < ApplicationController
   include ApplicationHelper
-  load_and_authorize_resource	
+  load_and_authorize_resource
   before_action :authenticate_user!
   before_action :load_info, :only => [:show, :print]
   before_action :check_status, :only => :update
-      
+
   def index
   	@search = Patient.ransack(params[:q])
   	@title = t('patient.index')
   	@patients = @search.result.order('cgdvcode DESC').page(params[:page])
   end
-  
+
   def show
   end
-  
+
   def notas
   	@patient = Patient.find(params[:id])
   	@search = @patient.notes.ransack(params[:q])
   	@notes = @search.result.order('folio DESC').page(params[:page]).per(10)
   end
-  
-  def print 
-  	render :layout => false 
+
+  def print
+  	render :layout => false
   end
-	
+
   def new
   	@patient = Patient.new
   	@cgdvcode = cgdvcode
-  	@title = t('helpers.submit.create', :model => Patient.to_s)
+  	@title = t('helpers.submit.create', model: Patient.model_name.human)
   end
-  
+
   def edit
     @patient = Patient.find(params[:id])
     @cgdvcode = @patient.cgdvcode
-    @title = t('helpers.submit.update', :model => Patient.to_s)
+    @title = t('helpers.submit.update', model: Patient.model_name.human)
   end
-  
+
   def create
       @patient = Patient.create(resource_params)
   	if @patient.save
-  	  flash[:success] = t('flash.success.create', :model => Patient.to_s)
+  	  flash[:success] = t('flash.success.create', model: Patient.model_name.human)
   	  redirect_to @patient
   	else
   	  @title = "New Patient"
@@ -47,24 +47,24 @@ class PatientsController < ApplicationController
   	  render 'new'
   	end
   end
-  
+
   def update
     if @patient.update(resource_params)
-      flash[:success] = t('flash.success.edit', :model => Patient.to_s)
+      flash[:success] = t('flash.success.edit', model: Patient.model_name.human)
       redirect_to @patient
     else
-      @title = t('helpers.submit.create', :model => Patient.to_s)
+      @title = t('helpers.submit.create', model: Patient.model_name.human)
       @cgdvcode = @patient.cgdvcode
       render 'edit'
     end
   end
-  
+
   def destroy
     Patient.find(params[:id]).destroy
-    flash[:success] = t('flash.success.destroy', :model => Patient.to_s)
+    flash[:success] = t('flash.success.destroy', model: Patient.model_name.human)
     redirect_to patients_path
   end
-  
+
   private
 
   # Paramaters that can be changed in the web forms
@@ -72,8 +72,8 @@ class PatientsController < ApplicationController
     params.require(:patient).permit(:name, :cgdvcode, :sex, :blod, :birthdate, :status, :montocon, :faviso, :fdefuncion)
   end
 
-  #Para evitar que los pacientes cambien status de REGLAMENTARIA  
-  def check_status	
+  #Para evitar que los pacientes cambien status de REGLAMENTARIA
+  def check_status
   	@patient = Patient.find(params[:id])
   	#Si el paciente tiene baja REGLAMENTARIA o es Menor de edad o tiene Adeudo y el usuario no es admin
   	#Reglamentaria
@@ -96,30 +96,30 @@ class PatientsController < ApplicationController
 	  if reglamentaria and !reglamentaria_nueva
 	    flash[:error] = t('patient.not', :s => "Baja Reglamentaria")
 	    error = true
-	  end  	
+	  end
 	  if activo and (!activo_nuevo and !activo_nuevo_defuncion) and menor
 	    flash[:error] = t('patient.not', :s => "Menor de Edad Nuevp")
-	    error = true      	
+	    error = true
 	  end
 	  #if activo and !activo_nuevo_defuncion and menor
 	  #  flash[:error] = t('patient.not', :s => "Menor de Edad Def")
-	  #  error = true      	
+	  #  error = true
 	  #end
 	  if activo and !activo_nuevo and adeudos
 	    flash[:error] = t('patient.not', :s => "Adeudo")
-	    error = true		
-	  end	
+	    error = true
+	  end
 	  if error
-		@title = t('helpers.submit.create', :model => Patient.to_s)
+		@title = t('helpers.submit.create', model: Patient.model_name.human)
 	    @cgdvcode = @patient.cgdvcode
 	    render 'edit'
 	    return false
 	  end
-	  
+
     end
       return true
   end
-  
+
   def load_info
     @patient = Patient.find(params[:id])
     @emails = @patient.emails
@@ -138,7 +138,7 @@ class PatientsController < ApplicationController
     @attachments = @patient.attachments
     @title = @patient.cgdvcode
   end
-  
+
     def cgdvcode
       if Patient.last == nil
   	    @cgdvcode = 1
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -18,17 +18,18 @@
 
 class Patient < ApplicationRecord
   BLOODTYPES = ['NS', 'A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-']
-  STATUS = [['Activo',1],['Abandono',2],['Reglamentaria',3],['Defuncion',4],['Remision',5],['Acceso Seguridad Social',6]]  
-  
+  STATUS = [['Activo',1],['Abandono',2],['Reglamentaria',3],['Defuncion',4],['Remision',5],['Acceso Seguridad Social',6]]
+  SEX = ['F','M']
+
   has_many :telephones, :as => :telephoneable, :dependent => :destroy
   has_many :addresses, :as => :addresseable, :dependent => :destroy
   has_many :addinfos, :as => :addinformation, :dependent => :destroy
   has_many :emails, :as => :emailable, :dependent => :destroy
   has_many :comments, :as => :commentable, :dependent => :destroy
   has_many :diagnosticos, :as => :diagnosticable, :dependent => :destroy
-  
+
   has_many :attachments, :as => :attachable, :dependent => :destroy
-    
+
   has_many :derechohabientes, :dependent => :destroy
   has_many :apoyos, :dependent => :destroy
   has_many :tratamientos, :dependent => :destroy
@@ -38,11 +39,11 @@ class Patient < ApplicationRecord
   has_one :refclinica, :dependent => :destroy
   has_one :house, :dependent => :destroy
   has_one :socioeco, :dependent => :destroy
-  
+
   validates :name, :presence => true
-  validates :cgdvcode, :presence => true, 
+  validates :cgdvcode, :presence => true,
   			:length => { :maximum => 20},
-			:numericality => true, 
+			:numericality => true,
             :uniqueness => true
   validates :sex, :presence => true, :length => { :maximum => 5}
   validates :blod, presence: true, length: { maximum: 5 }, inclusion: { :in => BLOODTYPES }
